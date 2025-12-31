74fdcf364b34535dc7b7b4c3f1fb68e209727b6a
Paulo Cesar <paulo.cardoso2408@gmail.com>
2025-12-31 14:49:22 -0300

    refactor: adicionado ajustes do reverb

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Events\EventTeste;
+use App\Events\EventExemple;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/web/{msg}', function($msg){
+    event(new EventExemple($msg));
+    return response()->json(['message' => 'Ok, funcionou!'], 200);
+}); 
